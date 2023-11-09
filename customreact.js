b62d3db74c9895b269ac2ef9ee2aefba20de85f6
Abinash-bit <abinashmahapatra41@gmail.com>
2023-11-09 20:06:37 +0530

    My own React Library

---
+++ customreact.js
@@ -0,0 +1,29 @@
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        if(prop === 'children'){
+            continue;
+        }
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+
+
+
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blanck'
+    },
+    children: 'Click here'
+}
+
+
+const mainContainer = Document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
